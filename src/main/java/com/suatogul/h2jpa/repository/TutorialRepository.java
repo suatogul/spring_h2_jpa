15804acb5a226635cef2a79dc894251fbcf01303
Suat OĞUL <suatogul@gmail.com>
2022-09-09 23:42:39 +0200

    all crud operations created

--- src/main/java/com/suatogul/h2jpa/repository/TutorialRepository.java
+++ src/main/java/com/suatogul/h2jpa/repository/TutorialRepository.java
@@ -1,4 +1,11 @@
 package com.suatogul.h2jpa.repository;
 
-public interface TutorialRepository {
+import com.suatogul.h2jpa.model.Tutorial;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface TutorialRepository extends JpaRepository<Tutorial, Long> {
+    List<Tutorial> findByPublished(boolean published);
+    List<Tutorial> findByTitleContaining(String title);
 }
